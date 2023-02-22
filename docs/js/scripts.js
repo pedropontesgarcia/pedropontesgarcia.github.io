f03bdb922ad40630f17d6a846964cf33094d38b3
Nyanar <pedropontesgr@gmail.com>
2023-02-22 10:24:15 -0500

    random splash

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,3 +1,10 @@
 function scrollOnClick() {
-  document.getElementById('content').scrollIntoView({ behavior: 'smooth' });
+	document.getElementById('content').scrollIntoView({ behavior: 'smooth' });
 }
+
+function randInt(min, max) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+rand = randInt(1, 3);
+document.querySelector('.right-pane').style.backgroundImage = "url('images/bg/" + rand + ".png')";
